f2fb477d09bdf41b38570f17d57579f2ed0799ab
Anderson Pires <andersonpires855@gmail.com>
2022-07-03 11:59:04 -0300

    Foi criada a classe course.service e ela foi declarada como injetavel

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Course } from "./course";
+import { CourseService } from "./course.service";
 
 @Component({
     selector: 'app-course-list',
@@ -7,32 +8,13 @@ import { Course } from "./course";
 })
 
 export class CourseListComponent implements OnInit {
+    
     courses: Course[] = [];
 
-    ngOnInit(): void{
-        this.courses = [
-            {
-                id: 1,
-                name: 'Angular: Forms',
-                imageUrl: './assets/images/forms.png',
-                price: 99.99,
-                code: 'XPS-9999',
-                duration: 120,
-                rating: 4.5,
-                releaseDate: 'December, 2, 2020'
-            },
-            {
-                id: 2,
-                name: 'Angular: HTTP',
-                imageUrl: './assets/images/http.png',
-                price: 55.55,
-                code: 'LKL-2584',
-                duration: 120,
-                rating: 4,
-                releaseDate: 'December, 2, 2020'
-            }
-        ]
-    }
+    constructor(private courseService: CourseService){}
 
+    ngOnInit(): void{
+        this.courses = this.courseService.retrieveAll();
+    }
 
 }
